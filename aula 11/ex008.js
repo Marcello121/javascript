adb87d59938a45c319a5e76a7823402cfb4ad762
Marcello121 <200999459+Marcello121@users.noreply.github.com>
2025-04-17 14:43:51 -0300

    att exS

---
+++ aula 11/ex008.js
@@ -0,0 +1,10 @@
+let vel = 12
+
+if (vel >= 100){
+    console.log('DIMINUI ANIMAL!')
+
+} else {
+    console.log('Não tem acelerador não?')
+}
+
+// Nesse exercício o guanabara fez com uma condição simples (sem else) mas como eu já tinha dado uma olhada em if e else, eu fiz um exemplo próprio pra ver se lembrava direito.
