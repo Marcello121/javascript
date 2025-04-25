5e5c80ec11706599d760b1a6292eeba3fae5d968
Marcello121 <200999459+Marcello121@users.noreply.github.com>
2025-04-24 23:50:58 -0300

    att ex

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,6 +1,13 @@
-let contador = 1
+// let contador = 1
 
-while (contador < 10){
-    console.log(`Tamo contando tamo contando ${contador}`)
-    contador++
+//while (contador < 10){
+   // console.log(`Tamo contando tamo contando ${contador}`)
+   // contador++
+//}
+
+for (let contador = 1;contador <= 10;contador++){
+    console.log(`Estamos a contar ${contador}`)
+
+    
 }
+
